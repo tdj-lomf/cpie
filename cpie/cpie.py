a86d23edf064376193d34601329343c2b1c59fc1
juntoda <lomf@hotmail.co.jp>
2020-03-05 22:02:44 +0900

    Refactoring of main module and add comments

--- cpie/cpie.py
+++ cpie/cpie.py
@@ -1,3 +1,7 @@
+"""Main module of the Clustering-based Promising Individual Enclosure
+providing class 'CPie'
+"""
+
 import math
 import numpy as np
 
@@ -6,12 +10,50 @@ from .solution import Solution
 
 
 class CPie:
+    """Clustering-based Promising Individual Enclosure.
+    Evolutionary computation algorithm which aims to search best
+    parameters minimizing black-box function, especially UV-function.
+    UV-function has U-valley, which occupies large search space but
+    contains only local optima, and V-valley, which occupies small
+    search space but contains global optima.
+    e.g. Double-sphere: f(x) = min((x-2)^2 + 0.1, 10*(x+2)^2)
 
-    def __init__(self, bounds_min, bounds_max, Ns=None, options={}):
+    Example Usage: Please see example_main.py
+        def main():
+            dimension = 2
+            bounds_min = [-10.0] * dimension
+            bounds_max = [ 10.0] * dimension
+            cpie = CPie(bounds_min, bounds_max, Ns=7*dimension)
+            for i in range(2000):
+                solution = cpie.sample()
+                f_value = objective_func(solution)
+                cpie.update(f_value)
+                cpie.print()
+            print("global best x", cpie.best.x)
+            print("global best f", cpie.best.f)
+            bests = cpie.get_bests()
+            for i, b in enumerate(bests):
+                print("mode", i, " f", b.f)
+
+    Returns:
+        List of Solution class -- Optimized solutions [s1, s2, ...]
+        Each solution has a parameter x and its evaluation value f.
+    """
+
+    def __init__(self, bounds_min, bounds_max, Ns=None, options=None):
+        """CPie constructor
+        Arguments:
+            bounds_min {list or numpy array} -- Minimum search range of X
+            bounds_max {list or numpy array} -- Maximum search range of X
+        Keyword Arguments:
+            Ns {int} -- Population size (default: {None})
+            options {dictionary} -- Additional options such as "max_mode" (default: {None})
+        """
+
         # problem settings
         self._dimension = len(bounds_min)
-        self._bounds_min = np.array([b_min for b_min in bounds_min])
-        self._bounds_max = np.array([b_max for b_max in bounds_max])
+        self._bounds_min = np.array(bounds_min)
+        self._bounds_max = np.array(bounds_max)
 
         # hyper parameters
         self._Ns = 7 * self._dimension if not Ns else Ns
@@ -27,10 +69,11 @@ class CPie:
 
         # additional options
         self._options = {
-            'max_mode': None,   # Max number of dividing enclosure, that is, number of final best solutions <= 'max_mode'.
+            'max_mode': None,   # Max num of dividing enclosure. Final best solutions <= 'max_mode'
         }
-        for key, value in options.items():
-            self._options[key] = value
+        if options:
+            for key, value in options.items():
+                self._options[key] = value
 
         # inner states
         self._enclosures = []
@@ -43,46 +86,69 @@ class CPie:
         self.iteration = 0
 
     def sample(self):
+        """Sample a solution that should be evaluated next
+        Returns:
+            numpy array -- Array of solution x
+        """
         if not self._enclosures:
             bounds_range = self._bounds_max - self._bounds_min
-            self._sample = np.random.random(self._dimension) * bounds_range + self._bounds_min
-            return self._sample
+            self._sample = np.random.random(
+                self._dimension) * bounds_range + self._bounds_min
         else:
             self._current_enclosure = self._select_enclosure()
-            self._sample = self._current_enclosure.sample(self._q, self._phi_max)
+            self._sample = self._current_enclosure.sample(
+                self._q, self._phi_max)
         return self._sample
 
     def update(self, f_value):
+        """Update CPIE inner states (The ellipsoid and population)
+        Arguments:
+            f_value {float} -- Evaluation value of the latest solution x
+        """
         self.iteration += 1
         self._f_value = f_value
         # update enclosure
         if not self._enclosures:    # initialization
             if f_value < float('inf'):
-                self._initial_solutions.append(Solution(self._sample.copy(), f_value))
+                self._initial_solutions.append(
+                    Solution(self._sample.copy(), f_value))
                 if len(self._initial_solutions) == self._Ns:
                     # generate first enclosure
                     enclosure = Enclosure(self._alpha, self._initial_solutions)
                     self._enclosures.append(enclosure)
         else:
-            self._current_enclosure.update(f_value, self._Ns, self._alpha, self._p, self._u)
+            self._current_enclosure.update(
+                f_value, self._Ns, self._alpha, self._p, self._u)
         if not self.best or f_value < self.best.f:
             self.best = Solution(self._sample.copy(), f_value)
-        
+
         # merge/divide enclosure
         if not self._current_enclosure:
             return
         if self._current_enclosure.merge(self._enclosures, self._Ns, self._zeta, self._alpha):
             return
         if not self._options['max_mode'] or len(self._enclosures) < self._options['max_mode']:
-            self._current_enclosure.divide(self._enclosures, self._o, self._rho, self._alpha)
+            self._current_enclosure.divide(
+                self._enclosures, self._o, self._rho, self._alpha)
 
     def print(self):
-        print("iter:", self.iteration, "f:", self._f_value, "f_best:", self.best.f, "num mode:", len(self._enclosures))
-    
+        """Print inner states (iteration, f, f_best, num mode)
+        """
+        print("iter:", self.iteration, "f:", self._f_value, "f_best:",
+              self.best.f, "num mode:", len(self._enclosures))
+
     def get_bests(self):
+        """Get best solutions of each mode
+        Returns:
+            list of Solution class -- Best solutions of each mode
+        """
         return [e.get_best() for e in self._enclosures]
 
     def _select_enclosure(self):
+        """Select enclosre which will sample next solution
+        Returns:
+            Enclosre class -- Selected enclosure which contains an ellipsoid and population
+        """
         if len(self._enclosures) == 1:
             selected = self._enclosures[0]
         elif np.random.random() < self._epsilon:
@@ -91,15 +157,19 @@ class CPie:
             selected = self._select_promising_enclosure()
         selected.add_t_count()
         return selected
-    
+
     def _select_promising_enclosure(self):
+        """Select promising enclosure from perspective of improvement rate
+        Returns:
+            Enclosure class -- An enclosre which has the highest improvement rate
+        """
         # if not enough data, then return the enclosure
         for e in self._enclosures:
             if len(e._average_history) < self._u:
                 return e
         # select by improvement rate
-        estimated_best = min(e.next_sample_estimation() for e in self._enclosures)
+        estimated_best = min(e.next_sample_estimation()
+                             for e in self._enclosures)
         for e in self._enclosures:
             e.calc_improvement_rate(estimated_best)
         return max(self._enclosures, key=lambda e: e.improvement_rate)
-
