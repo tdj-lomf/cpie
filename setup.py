97739d2c1aad5688e3ed913b74de6974ec0d3247
juntoda <lomf@hotmail.co.jp>
2020-03-06 22:21:00 +0900

    Add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cpie", # Replace with your own username
+    version="1.0.0",
+    author="Jun Toda",
+    author_email="lomf@hotmail.co.jp",
+    description="An evolutionary computation algorithm named CPIE in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tdj-lomf/cpie",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
